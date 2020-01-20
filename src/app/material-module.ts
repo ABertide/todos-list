ed1835b3974fb59d38f1aea86ef7623dc83f373d
ABertide <bertideallan@gmail.com>
2020-01-20 02:53:19 +0100

    setup config

---
+++ src/app/material-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+    exports: [MatToolbarModule],
+})
+export class MaterialModule {}
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
